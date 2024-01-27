9e70d4d81b7d4ddd96e247e9e40ef666972afcac
TonyMM55 <tonymmoran@gmail.com>
2024-01-27 16:34:56 +0000

    add square component

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,40 @@ import "./styles.css";
 //   );
 // }
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  // console.log({ value });
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
 
 // export default App
